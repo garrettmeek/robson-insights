ca8aa12a6750e6c706956c259f46b40c7474c1a6
garrettmeek <me@garrettmeek.com>
2024-12-04 20:27:50 -0500

    initial commit

--- robson_insight/asgi.py
+++ robson_insight/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+settings_module = 'robson_insight.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'robson_insight.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_asgi_application()
