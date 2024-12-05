ddf49e8bdbbcba7b8ff32f6b9bd0e2e46efe3cb1
garrettmeek <me@garrettmeek.com>
2024-12-04 23:29:50 -0500

    Adding superuser

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "robson_insight.settings")
+    settings_module = 'robson_insight.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'robson_insight.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
