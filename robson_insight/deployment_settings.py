a0506ad515c402d2a00af043e16114da697ee9e9
garrettmeek <me@garrettmeek.com>
2024-12-04 23:42:05 -0500

    Adding superuser

--- robson_insight/deployment_settings.py
+++ robson_insight/deployment_settings.py
@@ -60,17 +60,3 @@ LOGGING = {
         },
     },
 }
-
-
-
-ADMINS = [("CBI Analytics", "YOUREMAIL@EMAIL.com")]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'robsoninsights@gmail.com'
-EMAIL_HOST_PASSWORD = 'sush vvoe tlgx ikjl'
-# g-mail pass: 51f6p87kMaqr
-DEFAULT_FROM_EMAIL = 'robsoninsights@gmail.com'
