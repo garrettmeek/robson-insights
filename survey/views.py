5f6d01964d0e250219b40067f28bd3e0f46ca53c
garrettmeek <me@garrettmeek.com>
2024-12-11 21:54:38 -0500

    update

--- survey/views.py
+++ survey/views.py
@@ -2,6 +2,7 @@ import csv
 from django.core.exceptions import PermissionDenied
 from django.db.models import Q
 from django.utils.dateparse import parse_date
+from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponse, JsonResponse
 from django.core.mail import EmailMessage
@@ -26,20 +27,17 @@ from users.models import Group, UserProfile
 class EntryListView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = EntrySerializer
-    
+
     def get_queryset(self):
         user_profiles = UserProfile.objects.filter(
             user=self.request.user
         ).filter(Q(can_view=True) | Q(is_admin=True))
         allowed_groups = user_profiles.values_list('group', flat=True)
         return Entry.objects.filter(groups__in=allowed_groups).distinct()
-    
+
     def perform_create(self, serializer):
         # Fetch all user profiles where the user has can_add permissions
-        user_profiles = UserProfile.objects.filter(
-            user=self.request.user
-        ).filter(Q(can_add=True) | Q(is_admin=True))
-
+        user_profiles = UserProfile.objects
         if not user_profiles.exists():
             raise PermissionDenied("You do not have permission to add entries to any group.")
 
@@ -49,14 +47,14 @@ class EntryListView(generics.ListCreateAPIView):
         # Save the entry and associate it with all allowed groups
         entry = serializer.save(user=self.request.user)
         entry.groups.set(allowed_groups)
-    
-    
+
+
     def post(self, request, *args, **kwargs):
         if 'file' in request.FILES:
             return self.upload_file(request.FILES['file'])
         else:
             return super().post(request, *args, **kwargs)
-    
+
     def upload_file(self, file):
         try:
             _, file_extension = os.path.splitext(file.name)
@@ -64,15 +62,15 @@ class EntryListView(generics.ListCreateAPIView):
             if file_extension == '.csv':
                 with open(file, 'r') as file:
                     lines = file.readlines()
-                    
+
                 start_row = next(i for i, line in enumerate(lines) if line.strip().lower().startswith("group"))
                 df = pd.read_csv(file, skiprows=start_row, header=None)
-                
+
             elif file_extension == '.xlsx':
                 workbook = load_workbook(file, read_only=True)
                 sheet = workbook.active
-                
-                start_row = next(i for i, row in enumerate(sheet.iter_rows(values_only=True)) 
+
+                start_row = next(i for i, row in enumerate(sheet.iter_rows(values_only=True))
                                 if str(row[0]).strip().lower().startswith("group"))
                 df = pd.read_excel(file, skiprows=start_row, header=None)
             i = 1
@@ -93,7 +91,7 @@ class EntryListView(generics.ListCreateAPIView):
                         raise Exception
                 except:
                     return Response({'error': 'Invalid format'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-                
+
                 while df.iat[j, 0][:5] == "Group":
                     v = df.iat[j, i]
                     if not pd.isna(v):
@@ -111,7 +109,7 @@ class EntryListView(generics.ListCreateAPIView):
             return Response({"message": f"{count} entries uploaded successfully."}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
     def _create_entry(self, classification, csection, date):
         try:
             user = self.request.user
@@ -127,28 +125,28 @@ class EntryListView(generics.ListCreateAPIView):
             self.perform_create(serializer)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        
+
+
 class FilterEntriesByDateView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    
+
     def post(self, request):
         current_user = request.user
-        
+
         user_groups = UserProfile.objects.filter(user=current_user).values_list('group', flat=True)
-        
+
         if not user_groups.exists():
             return JsonResponse({'entries': []})
 
         start_date = request.POST.get('start_date', None)
         end_date = request.POST.get('end_date', None)
-        
+
         try:
             start_date = parse_date(start_date) if start_date else None
             end_date = parse_date(end_date) if end_date else None
         except ValueError:
             return JsonResponse({'error': 'Invalid date format'}, status=400)
-        
+
         if start_date:
             start_date = datetime.combine(start_date, datetime.min.time())
         if end_date:
@@ -185,10 +183,10 @@ class FilterEntriesByDateView(APIView):
             }
             for entry in entries
         ]
-        
+
         return JsonResponse({'entries': entries_data})
-        
-        
+
+
 class EntryFilterListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = EntrySerializer
@@ -197,10 +195,10 @@ class EntryFilterListView(generics.ListAPIView):
         # Exclude 'groups' field from the serializer
         kwargs['exclude_groups'] = True
         return super().get_serializer(*args, **kwargs)
-    
+
     def get_queryset(self):
         pk = self.kwargs.get('pk')
-        
+
         # Determine if the pk is prefixed with 'filter-' or 'group-'
         if pk.startswith('filter-'):
             filter_id = pk.split('-')[1]
@@ -221,7 +219,7 @@ class EntryFilterListView(generics.ListAPIView):
 
             groups_in_filter = user_filter.groups.filter(id__in=allowed_groups)
             return Entry.objects.filter(groups__in=groups_in_filter).distinct()
-        
+
         except Filter.DoesNotExist:
             return Entry.objects.none()
 
@@ -235,14 +233,14 @@ class EntryFilterListView(generics.ListAPIView):
                 raise PermissionDenied("You do not have permission to view entries for this group.")
 
             return Entry.objects.filter(groups__id=group_id).distinct()
-        
+
         except Group.DoesNotExist:
             return Entry.objects.none()
 
 class DownloadSurveyCSVView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = EntrySerializer
-    
+
     def get(self, request):
         try:
             queryset = Entry.objects.filter(
@@ -280,22 +278,22 @@ class DownloadSurveyCSVView(generics.ListCreateAPIView):
                 return Response({'message': 'CSV sent to email successfully!'}, status=status.HTTP_200_OK)
 
             return response
-            
+
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
 
 class EntryDetailView(generics.RetrieveAPIView):
     permission_classes = [CanReadEntry]
     serializer_class = EntrySerializer
-    
+
     def get_queryset(self):
-        
+
         pk = self.kwargs.get('pk')
         queryset = Entry.objects.get(pk=pk)
         return queryset
-    
-    
+
+
 class FilterConfigurationListCreateView(generics.ListCreateAPIView):
     serializer_class = FilterSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -305,7 +303,7 @@ class FilterConfigurationListCreateView(generics.ListCreateAPIView):
         # Get groups the user has permission to view
         user_profiles = UserProfile.objects.filter(user=self.request.user, can_view=True)
         allowed_groups = user_profiles.values_list('group', flat=True)
-        
+
         # Include filters with no groups or with allowed groups
         return user_filters.filter(Q(groups__in=allowed_groups) | Q(groups__isnull=True)).distinct()
 
@@ -313,7 +311,7 @@ class FilterConfigurationListCreateView(generics.ListCreateAPIView):
         groups = serializer.validated_data.get('groups', [])
 
         user_groups = Group.objects.filter(userprofile__user=self.request.user)
-        
+
         user_profiles = UserProfile.objects.filter(user=self.request.user, can_view=True)
         allowed_groups = user_profiles.values_list('group', flat=True)
 
@@ -327,7 +325,7 @@ class CreateConfiguration(APIView):
 
     def post(self, request):
         configuration_name = request.data.get('configuration_name', '').strip()
-        
+
         if not configuration_name:
             return Response({'error': 'Configuration name is required'}, status=status.HTTP_400_BAD_REQUEST)
         if len(configuration_name) > 100:
@@ -365,10 +363,10 @@ class AddGroupToConfiguration(APIView):
             configuration.groups.add(group)
 
             return Response({'success': f'{group.name} added to configuration {configuration.name}'}, status=status.HTTP_200_OK)
-        
+
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
 class RemoveGroupFromConfiguration(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -385,7 +383,7 @@ class RemoveGroupFromConfiguration(APIView):
             configuration.groups.remove(group)
 
             return Response({'success': f'{group.name} removed from configuration {configuration.name}'}, status=status.HTTP_200_OK)
-        
+
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -402,7 +400,7 @@ class FilterConfigurationDetailView(generics.ListAPIView):
         filter_instance = self.get_queryset().first()
 
         group_ids = list(filter_instance.groups.values_list('id', flat=True))
-        
+
         return Response(group_ids)
 
 class GenerateQuarterlyXLSX(APIView):
@@ -445,7 +443,7 @@ class GenerateQuarterlyXLSX(APIView):
 
         ws.append(headers)
         ws.append(subheaders)
-       
+
         ws.merge_cells(start_row=1, start_column=1, end_row=2, end_column=1)
         ws["A1"].alignment = Alignment(horizontal="center", vertical="center")
         ws["A1"].font = Font(bold=True)
@@ -518,7 +516,9 @@ class GenerateQuarterlyXLSX(APIView):
 class DeleteFilterView(APIView):
     
     def delete(self, request, pk):
-                
+        
+        filter = get_object_or_404(Filter, pk=pk)
+        
         try:
             filter = Filter.objects.get(pk=pk, user=request.user)
         except:
